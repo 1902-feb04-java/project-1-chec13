7e1ba3cb0a13074f512aa5092525ae02d738c4e6
John Vasgird <38052305+chec13@users.noreply.github.com>
2019-02-25 14:27:36 -0600

    added employees class to java

--- project1/src/main/java/com/project1/John/App.java
+++ project1/src/main/java/com/project1/John/App.java
@@ -1,40 +1,28 @@
 package com.project1.John;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.sql.ResultSet;
 
 /**
  * Hello world!
  *
  */
 public class App {
-    public static void main(String[] args) {
-        Scanner scr = new Scanner(System.in);
-        System.out.print("username: ");
-        String s = scr.nextLine();
-
-        System.out.print("\nPassword: ");
-        s = scr.next();
-        System.out.print("\n");
-        //scr.close();
+    public static void main(String[] args) 
+    {
+    	Employees emp = Employees.getEmployee(1);
+    	System.out.println(emp.firstName);
         
-        InputStreamReader isr = new InputStreamReader(System.in);
-        BufferedReader br = new BufferedReader(isr);
-        //scr.close();
 
-        System.out.print("\nusername: ");
-        try {
-            s = br.readLine();
-            System.out.print("\nPassword: ");
-            s = br.readLine();
-            br.close();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        scr.close();
+        
+        
+       
 
 
     }
